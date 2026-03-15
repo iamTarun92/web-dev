d678d0b1e782d9d4cb7668821d9bc000eee16067
Tarun <iamTarunPandey@outlook.com>
2026-03-16 00:51:05 +0530

    rename file name from validation to validator

---
+++ user-roles-permission-express-mongodb-app/helpers/validator.js
@@ -0,0 +1,29 @@
+const { check } = require('express-validator')
+
+const registerValidator = [
+  check('name', 'Name is required.').not().isEmpty(),
+  check('email', 'Please include a valid email.').isEmail().normalizeEmail({
+    gmail_remove_dots: true,
+  }),
+  check(
+    'password',
+    'Password must be greater than 6 characters, and contains at least one uppercase letter, one lowercase letter, and one number, and one special character.',
+  ).isStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minLowercase: 1,
+    minNumbers: 1,
+  }),
+]
+
+const loginValidator = [
+  check('email', 'Please include a valid email.').isEmail().normalizeEmail({
+    gmail_remove_dots: true,
+  }),
+  check('password', 'Password is required.').not().isEmpty(),
+]
+
+module.exports = {
+  registerValidator,
+  loginValidator,
+}
